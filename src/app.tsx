2325e1a4bb4dd8a1f2424da213b755773b67485d
welsi <wesleyp@hey.com>
2021-12-26 12:03:41 +0100

    lol add the rest

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { Logo } from './components/logo'
+
+export function App() {
+  return (
+    <>
+      <div>
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="text-white text-7xl"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+      </div>
+    </>
+  )
+}
